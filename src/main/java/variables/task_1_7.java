183616ce633f7b3f2a0e03d3bc7739af7c31ea52
Pavel Garmatyuk <m-krum@yandex.ru>
2021-09-08 21:13:11 +0300

    Variable tasks is done

---
+++ src/main/java/variables/task_1_7.java
@@ -0,0 +1,12 @@
+package variables;
+/** Задание 1.7
+ Напишите фрагмент класса, который присваивает переменной х целочисленное значение,
+ а затем присваивает переменной у треть от значения переменной х.
+ */
+public class task_1_7 {
+    public static void main(String[] args) {
+        int x = 9;
+        int y = x / 3;
+        System.out.println(y);
+    }
+}
