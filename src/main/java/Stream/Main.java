be1b7ce7bdd612b2a4ab702fc8aabe772a7ea3fd
Pavel Garmatyuk <m-krum@yandex.ru>
2021-12-08 11:08:16 +0300

    short writing of lambdas func is done

--- src/main/java/Stream/Main.java
+++ src/main/java/Stream/Main.java
@@ -10,21 +10,17 @@ public class Main {
     public static void main(String[] args) {
         List<Integer> numbers = new ArrayList<>();
         for (int i = 0; i < 100; i++) {
-            numbers.add((int) (Math.random() * 1000));
+            numbers.add((int) (Math.random() * 100 + 100));
         }
 
-
-        List<String> list = numbers.stream()
-                .filter((n) -> n % 2 == 0)
-                .map((integer) -> "Number: " + integer)
-                .filter((string) -> string.endsWith("0"))
-                .map((string) -> string + "!")
+        List<String> result = numbers.stream()
+                .filter((integer -> integer % 2 == 0 && integer % 5 == 0))
+                .map((Math::sqrt))
+                .map((sqrt) -> "Sqrt: " + sqrt)
                 .collect(Collectors.toList());
-
-        for (String s: list) {
+        for (String s: result) {
             System.out.println(s);
         }
-
     }
 
     private static List<String> map(List<Integer> numbers) {
