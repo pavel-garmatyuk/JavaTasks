b1ff10f52a6f26ab20430f8171e5c969b9861a06
Pavel Garmatyuk <m-krum@yandex.ru>
2021-12-08 10:45:08 +0300

    stream mapping is done

--- src/main/java/Stream/Main.java
+++ src/main/java/Stream/Main.java
@@ -10,12 +10,25 @@ public class Main {
             numbers.add((int) (Math.random() * 1000));
         }
 
-
         List<Integer> filtered = filter(numbers, (n) -> n % 2 == 0);
-            for (int i : filtered) {
+        for (int i : filtered) {
+            System.out.println(i);
+        }
+
+        List<String> mapped = map(filtered);
+        for (String i: mapped) {
             System.out.println(i);
         }
     }
+
+    private static List<String> map(List<Integer> numbers) {
+        List<String> result = new ArrayList<>();
+        for (int number : numbers) {
+            result.add("Number: " + number);
+        }
+        return result;
+    }
+
     private static List<Integer> filter(List<Integer> list, Predicate predicate) {
         List<Integer> result = new ArrayList<>();
         for (int i : list) {
